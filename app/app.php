51976b284c7577f664551150503a40ae95cbceee
Jessica Fix and Mary Warrington <marywarrington@gmail.com>
2016-02-15 08:45:25 -0800

    Add silex and twig

--- app/app.php
+++ app/app.php
@@ -1,10 +1,21 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/TitleCaseGenerator.php";
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Home";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig');
+    });
+
+    $app->get("/view_title_case", function() use ($app) {
+        $my_TitleCaseGenerator = new TitleCaseGenerator;
+        $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
+        return $app['twig']->render('results.html.twig', array('result'=> $title_cased_phrase));
     });
 
     return $app;
